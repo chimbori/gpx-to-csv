509460d9692a52d8f884ac439cadd8a0681bd606
Chimbori <hello@chimbori.com>
2026-01-14 17:10:08 -0800

    Move most logic to `convert.go` from `main.go`

---
+++ src/convert.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"log/slog"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/lmittmann/tint"
+)
+
+func convertGpxFile(files []string) error {
+	w := csv.NewWriter(os.Stdout)
+	defer w.Flush()
+
+	csvRow := []string{"SourceFile", "GPSDateTime", "GPSLatitude", "GPSLatitudeRef", "GPSLongitude", "GPSLongitudeRef"}
+	if err := w.Write(csvRow); err != nil {
+		slog.Error("error writing record to csv", tint.Err(err))
+		os.Exit(1)
+	}
+
+	for _, file := range files {
+		slog.Info(file)
+
+		gpxBytes, err := os.ReadFile(file)
+		if err != nil {
+			return fmt.Errorf("error reading file: %w", err)
+		}
+
+		gpx, err := parseGpx(gpxBytes)
+		if err != nil {
+			return fmt.Errorf("error parsing file: %w", err)
+		}
+
+		for _, track := range gpx.Tracks {
+			for _, segment := range track.Segments {
+				for _, point := range segment.Points {
+					localTimestamp := utcToLocal(point.Timestamp)
+					csvRow = []string{
+						"./" + localTimestamp + ".jpg",   // SourceFile,
+						localTimestamp,                   // GPSDateTime
+						precision7digit(point.Latitude),  // GPSLatitude
+						latitudeRef(point.Latitude),      // GPSLatitudeRef
+						precision7digit(point.Longitude), // GPSLongitude
+						longitudeRef(point.Longitude),    // GPSLongitudeRef
+					}
+					if err := w.Write(csvRow); err != nil {
+						return fmt.Errorf("error writing CSV: %w", err)
+					}
+				}
+			}
+		}
+	}
+
+	return nil
+}
+
+func utcToLocal(utcTimeStr string) string {
+	utcTime, err := time.Parse(time.RFC3339, utcTimeStr)
+	if err != nil {
+		slog.Error("error parsing timestamp", tint.Err(err), "timestamp", utcTimeStr)
+		return utcTimeStr // Return original if parsing fails.
+	}
+	return utcTime.Local().Format(time.RFC3339)
+}
+
+func precision7digit(f float64) string { return strconv.FormatFloat(f, 'f', 7, 64) }
+
+func latitudeRef(lat float64) string {
+	if lat > 0 {
+		return "North"
+	} else {
+		return "South"
+	}
+}
+
+func longitudeRef(lon float64) string {
+	if lon > 0 {
+		return "East"
+	} else {
+		return "West"
+	}
+}
--- src/main.go
+++ src/main.go
@@ -1,14 +1,11 @@
 package main
 
 import (
-	"encoding/csv"
 	"flag"
 	"fmt"
 	"log/slog"
 	"os"
 	"path/filepath"
-	"strconv"
-	"time"
 
 	"github.com/lmittmann/tint"
 )
@@ -24,82 +21,9 @@ func main() {
 		os.Exit(1)
 	}
 
-	err := convertFile(flag.Args())
+	err := convertGpxFile(flag.Args())
 	if err != nil {
 		slog.Error("conversion failed", tint.Err(err))
 		os.Exit(1)
-	}
-}
-
-func convertFile(files []string) error {
-	w := csv.NewWriter(os.Stdout)
-	defer w.Flush()
-
-	csvRow := []string{"SourceFile", "GPSDateTime", "GPSLatitude", "GPSLatitudeRef", "GPSLongitude", "GPSLongitudeRef"}
-	if err := w.Write(csvRow); err != nil {
-		slog.Error("error writing record to csv", tint.Err(err))
-		os.Exit(1)
-	}
-
-	for _, file := range files {
-		slog.Info(file)
-
-		gpxBytes, err := os.ReadFile(file)
-		if err != nil {
-			return fmt.Errorf("error reading file: %w", err)
-		}
-
-		gpx, err := parseGpx(gpxBytes)
-		if err != nil {
-			return fmt.Errorf("error parsing file: %w", err)
-		}
-
-		for _, track := range gpx.Tracks {
-			for _, segment := range track.Segments {
-				for _, point := range segment.Points {
-					localTimestamp := utcToLocal(point.Timestamp)
-					csvRow = []string{
-						"./" + localTimestamp + ".jpg",   // SourceFile,
-						localTimestamp,                   // GPSDateTime
-						precision7digit(point.Latitude),  // GPSLatitude
-						latitudeRef(point.Latitude),      // GPSLatitudeRef
-						precision7digit(point.Longitude), // GPSLongitude
-						longitudeRef(point.Longitude),    // GPSLongitudeRef
-					}
-					if err := w.Write(csvRow); err != nil {
-						return fmt.Errorf("error writing CSV: %w", err)
-					}
-				}
-			}
-		}
-	}
-
-	return nil
-}
-
-func utcToLocal(utcTimeStr string) string {
-	utcTime, err := time.Parse(time.RFC3339, utcTimeStr)
-	if err != nil {
-		slog.Error("error parsing timestamp", tint.Err(err), "timestamp", utcTimeStr)
-		return utcTimeStr // Return original if parsing fails.
-	}
-	return utcTime.Local().Format(time.RFC3339)
-}
-
-func precision7digit(f float64) string { return strconv.FormatFloat(f, 'f', 7, 64) }
-
-func latitudeRef(lat float64) string {
-	if lat > 0 {
-		return "North"
-	} else {
-		return "South"
-	}
-}
-
-func longitudeRef(lon float64) string {
-	if lon > 0 {
-		return "East"
-	} else {
-		return "West"
 	}
 }
