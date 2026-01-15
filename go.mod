564fc8428347502d0fdf9a4d56dada9b47c75ce4
Chimbori <hello@chimbori.com>
2026-01-14 16:55:22 -0800

    Use `slog` instead of `slog` for logging

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module chimbori.dev/gpx-to-csv
 
 go 1.23.5
+
+require github.com/lmittmann/tint v1.1.2
--- main.go
+++ main.go
@@ -4,14 +4,19 @@ import (
 	"encoding/csv"
 	"flag"
 	"fmt"
-	"log"
+	"log/slog"
 	"os"
 	"path/filepath"
 	"strconv"
 	"time"
+
+	"github.com/lmittmann/tint"
 )
 
 func main() {
+	tintHandler := tint.NewHandler(os.Stderr, &tint.Options{TimeFormat: "2006-01-02 15:04:05.000"})
+	slog.SetDefault(slog.New(tintHandler))
+
 	flag.Parse()
 
 	if len(flag.Args()) == 0 {
@@ -26,15 +31,16 @@ func parseGpx(files []string) {
 
 	csvRow := []string{"SourceFile", "GPSDateTime", "GPSLatitude", "GPSLatitudeRef", "GPSLongitude", "GPSLongitudeRef"}
 	if err := w.Write(csvRow); err != nil {
-		log.Fatalln("error writing record to csv:", err)
+		slog.Error("error writing record to csv", tint.Err(err))
+		os.Exit(1)
 	}
 
 	for _, file := range files {
-		log.Println(file)
+		slog.Info(file)
 		gpxBytes, err := os.ReadFile(file)
 		gpx, err := ParseBytes(gpxBytes)
 		if err != nil {
-			log.Println(err)
+			slog.Error("parse error", tint.Err(err))
 		}
 
 		for _, track := range gpx.Tracks {
@@ -50,7 +56,8 @@ func parseGpx(files []string) {
 						longitudeRef(point.Longitude),    // GPSLongitudeRef
 					}
 					if err := w.Write(csvRow); err != nil {
-						log.Fatalln("error writing record to csv:", err)
+						slog.Error("error writing record to csv", tint.Err(err))
+						os.Exit(1)
 					}
 				}
 			}
@@ -60,14 +67,15 @@ func parseGpx(files []string) {
 	w.Flush()
 
 	if err := w.Error(); err != nil {
-		log.Fatal(err)
+		slog.Error("csv flush error", tint.Err(err))
+		os.Exit(1)
 	}
 }
 
 func utcToLocal(utcTimeStr string) string {
 	utcTime, err := time.Parse(time.RFC3339, utcTimeStr)
 	if err != nil {
-		log.Printf("error parsing timestamp %s: %v", utcTimeStr, err)
+		slog.Error("error parsing timestamp", tint.Err(err), "timestamp", utcTimeStr)
 		return utcTimeStr // Return original if parsing fails.
 	}
 	return utcTime.Local().Format(time.RFC3339)
